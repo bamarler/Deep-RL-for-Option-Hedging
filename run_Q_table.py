45ba530d2a803b5eebd61be74848f2767877d478
bamarler <bam2738@gmail.com>
2025-06-14 19:16:05 -0400

    added script to run generated Q-table

---
+++ run_Q_table.py
@@ -0,0 +1,20 @@
+import sys
+import pickle
+import numpy as np
+from Q_learning import hash
+from ..option_gym import OptionEnv
+
+with open('QLearning/Q_table.pickle', 'rb') as handle:
+    Q_table = pickle.load(handle)
+
+env = OptionEnv(tickers=['AAPL', 'MSFT', 'IBM', 'JNJ', 'MCD', 
+           'KO', 'PG', 'WMT', 'XOM', 'GE', 
+           'MMM', 'F', 'T', 'CSCO', 'PFE',
+           'INTC', 'BA', 'CAT', 'CVX', 'PEP'])
+
+obs, _ = env.reset()
+
+while not env.done:
+    state = hash(obs)
+    action = np.argmax(Q_table[state])
+    obs, reward, done, truncated, _ = env.step(action)
